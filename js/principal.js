16dd42b8c19ebc7d57a4d7d34d51baf6b5b113e7
felipecapelli <felipecapelli_uru@hotmail.com>
2021-10-06 23:45:16 -0300

    Aula 2 - Variaveis e operadores

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,35 @@
 var titulo = document.querySelector(".titulo");
-console.log(titulo);
-console.log(titulo.textContent);
+//console.log(titulo);
+//console.log(titulo.textContent);
 
 titulo.textContent = "Aparecida Nutricionista";	
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso invalido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso invalido!";
+}
+
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+
+if(pesoEhValido && alturaEhValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
