6ce0f78e5cc279aec135b46d29895fc52a1056c0
felipecapelli <felipecapelli_uru@hotmail.com>
2021-10-21 22:39:24 -0300

    Aula 3 - Array, Loop e estilos

--- js/principal.js
+++ js/principal.js
@@ -4,32 +4,39 @@ var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista";	
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
+for (let i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var tdImc = paciente.querySelector(".info-imc");
 
-if(peso <= 0 || peso >= 1000){
-    console.log("Peso invalido");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso invalido!";
-}
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
-if(altura <= 0 || altura >= 3.00){
-    console.log("Altura inválida");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida!";
-}
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso invalido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso invalido!";
+        //paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido");
+    }
 
+    if(altura <= 0 || altura >= 3.00){
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
+    }
 
-if(pesoEhValido && alturaEhValida){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if(pesoEhValido && alturaEhValida){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+    
 }
